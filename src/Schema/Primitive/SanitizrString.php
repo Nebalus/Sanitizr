3666e7565694c80dc2cfdfebc2833754d2bc4036
Nebalus <56486387+Nebalus@users.noreply.github.com>
2026-03-18 21:24:57 +0100

    Update src/Schema/Primitive/SanitizrString.php
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/Schema/Primitive/SanitizrString.php
+++ src/Schema/Primitive/SanitizrString.php
@@ -470,7 +470,7 @@ class SanitizrString extends AbstractSanitizrSchema
     {
         $newSchema = clone $this;
         $newSchema->addCheck(function (string $input, string $path) use ($message) {
-            if (! preg_match('/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/i', $input)) {
+            if (! preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-[1-8][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i', $input)) {
                 throw SanitizrValidationException::fromIssue(new SanitizrIssue(
                     code: SanitizrIssue::INVALID_STRING,
                     path: self::pathToArray($path),
