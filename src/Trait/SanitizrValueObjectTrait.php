690d9316f01081328976d6e386f75071fd9b2dd6
nebalus <coding@nebalus.dev>
2026-09-05 18:17:06 +0200

    dev: Added IsValid Methode in SanitizrValueObjectTrait.php

--- src/Trait/SanitizrValueObjectTrait.php
+++ src/Trait/SanitizrValueObjectTrait.php
@@ -17,5 +17,20 @@ trait SanitizrValueObjectTrait
         return clone self::$schemaCache;
     }
 
+    /**
+     * Checks whether the given value satisfies the schema of this value object.
+     *
+     * Only the validity of the input is reported; any exception thrown while building
+     * the schema itself is propagated, since that indicates a faulty schema definition
+     * rather than an invalid input value.
+     *
+     * @param mixed $value The value to validate against the schema.
+     * @return bool True if the value satisfies the schema, false otherwise.
+     */
+    public static function isValid(mixed $value): bool
+    {
+        return static::getSchema()->safeParse($value)->isValid();
+    }
+
     abstract protected static function defineSchema(): AbstractSanitizrSchema;
 }
